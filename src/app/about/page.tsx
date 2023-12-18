17cf4d6f07eebbf4e10f7ecb6c7e73f0b2bee6c6
AlexCS1337 <alexander.carlos.silva@gmail.com>
2023-12-18 18:55:25 +1000

    Update webpages to be more consistant

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,87 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+
 export default function About() {
-    return <div>About</div>
-  }
+  return (
+    <main className={styles.main}>
+      <div className={styles.info_about}>
+        <p>Text goes here</p>
+      </div>
+
+        <div className={styles.grid}>
+        <a
+          href="/about"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            About <span>-&gt;</span>
+          </h2>
+          <p>
+            Find in-depth information about Code Tensai and what they offer.
+          </p>
+        </a>
+
+        <a
+          href="/currentprojects"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Current Projects <span>-&gt;</span>
+          </h2>
+          <p>Check out current projects being worked on</p>
+        </a>
+
+        <a
+          href="/examples"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Examples <span>-&gt;</span>
+          </h2>
+          <p>Some examples of how your website can look!</p>
+        </a>
+
+        <a
+          href="/contact"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Contact <span>-&gt;</span>
+          </h2>
+          <p>
+            Contact now for a quote or more information.
+          </p>
+        </a>
+      </div>
+
+      <div className={styles.description}>
+        <p>This is a work in progress website</p>
+        <div>
+          <a
+            href="https://codetensai.com.au"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            2023 ©{" "}
+            <Image
+              src="/codetensai-logo.png"
+              alt="Code Tensai Logo"
+              // className={styles.codetensaiLogo}
+              width={100}
+              height={50}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+    </main>
+  );
+}
